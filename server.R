b0a4824284a69691dd6e3321ba26a41f71e87f39
Victor Ordu <victorordu@outlook.com>
2026-01-13 17:19:35 -0500

    Introduce {latex2r} to the server function

--- server.R
+++ server.R
@@ -17,7 +17,7 @@ server <- function(input, output, session) {
   # only when the actionButton is clicked
   result <- eventReactive(input$go, {
     req(input$xmin, input$xmax)
-    expr_string <- process_parsed_expr(input$expr)
+    expr_string <- latex2r::latex2r(input$expr)
     evaluate_expr(expr_string, input$xmin, input$xmax)
   })
 
