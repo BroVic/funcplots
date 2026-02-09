8b72fc6694879886db66c3f80fc4dd6ec3b5cbb4
Victor Ordu <victorordu@outlook.com>
2026-02-09 15:52:55 -0500

    Add new tests (shinytest2)

--- ui.R
+++ ui.R
@@ -1,36 +1,36 @@
 library(shiny)
+library(bslib)
 
-ui <- fluidPage(
-  sidebarLayout(
-    sidebarPanel(
-      withMathJax(),
+ui <- page_sidebar(
+  plotOutput("plot"),
+  
+  colourpicker::colourInput(
+    inputId = "color",
+    label = "Pick a color",
+    value = "black",
+    showColour = "background",
+    palette = "limited",
+    width = "30%"
+  ), 
+  
+  sliderInput("linewidth", "Linewidth", 1, 10, 1, ticks = FALSE),
+  
+  input_dark_mode(id = 'theme'),
+  
+  sidebar = sidebar(
+    withMathJax(),
+    
+    textInput("expr", "Enter an expression with variable 'x'", "x^2"),
+    
+    uiOutput("equation"),
+    
+    hr(),
+   
+    layout_columns(
+      numericInput("xmin", "minimum", -5, width = "70%"),
+      numericInput("xmax", "maximum", 5, width = "70%")
+    ), 
       
-      textInput("expr", "Enter an expression with variable 'x'", "x^2"),
-      
-      uiOutput("equation"),
-      
-      hr(),
-      
-      fluidRow(
-        column(width = 6, numericInput("xmin", "minimum", -5, width = "70%")),
-        column(width = 6, numericInput("xmax", "maximum", 5, width = "70%"))
-      ),
-      
-      actionButton("go", "Plot!")
-    ),
-    mainPanel(
-      plotOutput("plot"),
-      
-      colourpicker::colourInput(
-        inputId = "color",
-        label = "Pick a color",
-        value = "black",
-        showColour = "background",
-        palette = "limited",
-        width = "30%"
-      ), 
-      
-      sliderInput("linewidth", "Linewidth", 1, 10, 1, ticks = FALSE)
-    )
+    actionButton("go", "Plot!")
   )
 )
